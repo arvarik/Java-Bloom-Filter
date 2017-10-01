9597694d96cd6b83d2e25fdb684b87fea510d085
Arvind Arikatla <arvind.arik@jhu.edu>
2017-10-01 13:00:06 -0700

    Added skeleton for bloom filter implementation class

--- src/main/java/model/bloomfilter/BloomFilter.java
+++ src/main/java/model/bloomfilter/BloomFilter.java
@@ -1,5 +1,6 @@
 package model.bloomfilter;
 
+import java.util.HashSet;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -8,7 +9,7 @@ interface BloomFilter {
 
     /**
      * Checks the bloom filter to see status of key
-     * @param key Term to check in the bloom filter
+     * @param key Term to check in the bloomfilter
      * @return Response if key was probably in the set or definitely not in the set
      */
     String checkTerm(String key);
@@ -16,14 +17,14 @@ interface BloomFilter {
 
     /**
      * Adds a term to the bloom filter
-     * @param key Term to add into the bloom filter
+     * @param key Term to add into the bloomfilter
      */
     void addTerm(String key);
 
 
     /**
      * Returns all the hashed outputs of term
-     * @param key Term to check in bloom filter
+     * @param key Term to check in bloomfilter
      * @return List of bit indices corresponding to term
      */
     ArrayList<Integer> getBitIndices(String key);
@@ -37,7 +38,7 @@ interface BloomFilter {
 
 
     /**
-     * Returns the total in-memory storage of bloom filter in bits
+     * Returns the total in-memory storage of bloomfilter in bits
      * @return Size of bloom filter
      */
     int getBloomFilterSize();
@@ -48,6 +49,20 @@ interface BloomFilter {
      * @return Number of hash functions
      */
     int getNumHashFunctions();
+
+
+    /**
+     * Returns the number of terms in the bloomfilter
+     * @return
+     */
+    int getNumTerms();
+
+
+    /**
+     * Returns a hash set of terms in the bloomfilter
+     * @return Hash set of strings
+     */
+    HashSet<String> getTerms();
 
 
     /**
--- src/main/java/model/bloomfilter/DefaultBloomFilter.java
+++ src/main/java/model/bloomfilter/DefaultBloomFilter.java
@@ -1,9 +1,113 @@
 package model.bloomfilter;
 
-public class DefaultBloomFilter implements BloomFilter {
+import java.util.ArrayList;
+import java.util.BitSet;
+import java.util.HashMap;
+import java.util.HashSet;
+
+
+class DefaultBloomFilter implements BloomFilter {
+
+    int bloomFilterSize;    // Referred to in stats as m
+    int numTerms;           // Referred to in stats as n
+    int numHashFunctions;   // Referred to in stats as k
+
+    BitSet bitVector;       // Bit vector of bloomfilter
+
+
 
 
     DefaultBloomFilter() {
+
+    }
+
+
+    /**
+     * Checks the bloom filter to see status of key
+     *
+     * @param key Term to check in the bloomfilter
+     * @return Response if key was probably in the set or definitely not in the set
+     */
+    public String checkTerm(String key) {
+        return null;
+    }
+
+
+    /**
+     * Adds a term to the bloom filter
+     *
+     * @param key Term to add into the bloomfilter
+     */
+    public void addTerm(String key) {
+
+    }
+
+
+    /**
+     * Returns all the hashed outputs of term
+     *
+     * @param key Term to check in bloomfilter
+     * @return List of bit indices corresponding to term
+     */
+    public ArrayList<Integer> getBitIndices(String key) {
+        return null;
+    }
+
+
+    /**
+     * Gets a few statistics of the current bloomfilter
+     *
+     * @return Hash map of statistic name and its associated value
+     */
+    public HashMap<String, String> getBloomFilterStats() {
+        return null;
+    }
+
+
+    /**
+     * Returns the total in-memory storage of bloomfilter in bits
+     *
+     * @return Size of bloom filter
+     */
+    public int getBloomFilterSize() {
+        return 0;
+    }
+
+
+    /**
+     * Returns total number of hash functions applied to each string
+     *
+     * @return Number of hash functions
+     */
+    public int getNumHashFunctions() {
+        return 0;
+    }
+
+
+    /**
+     * Returns the number of terms in the bloomfilter
+     *
+     * @return
+     */
+    public int getNumTerms() {
+        return 0;
+    }
+
+
+    /**
+     * Returns a hash set of terms in the bloomfilter
+     *
+     * @return Hash set of strings
+     */
+    public HashSet<String> getTerms() {
+        return null;
+    }
+
+
+    /**
+     * Clears the bloom filter of all strings
+     */
+    public void clearBloomFilter() {
 
     }
 
